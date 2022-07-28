8ad4ab019fb8f702c8acaa5ffcb79c2c722d1164
princerohan225 <princerohan225@gmail.com>
2022-07-28 18:01:47 +0530

    Updates

---
+++ precache-manifest.ce4bec0c5bcdf3a0087506cf769234b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ffcf9307bb53e496c77",
+    "url": "/TextUtils-React/static/css/main.5fc1edb8.chunk.css"
+  },
+  {
+    "revision": "5ffcf9307bb53e496c77",
+    "url": "/TextUtils-React/static/js/main.5ffcf930.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtils-React/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "330e91201df57b208926",
+    "url": "/TextUtils-React/static/js/runtime~main.330e9120.js"
+  },
+  {
+    "revision": "b9f048ce24149eaefb4c52b38be35c6b",
+    "url": "/TextUtils-React/index.html"
+  }
+];
